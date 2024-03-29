b69ea0838b864efbf857de1c887589e82ccfab57
YasmineZerai <zeraiyasmine7@gmail.com>
2024-03-29 22:01:49 +0100

    added footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+<section style="width: 100vw; background-color: #0B001A">
+   <div style="display: flex; color:white; justify-content: center;  align-items: center; gap:100px" >
+      <div>
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +216 90 965 765 </p>
+         <p> <i class="fas fa-phone"></i> +216 90 965 865 </p>
+         <p> <i class="fas fa-envelope"></i> techHive@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Tunis-Tunisia - 400104 </p>
+      </div>
+
+      <div style="display:flex; flex-direction: column; gap:10px" >
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+</section>
