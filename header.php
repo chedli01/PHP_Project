c6fce171a235745f454c06083350c1834ad07b53
YasmineZerai <zeraiyasmine7@gmail.com>
2024-03-29 22:01:27 +0100

    added header

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+            <img class="image" src="images\Capture_d_écran_2024-03-28_203421-removebg-preview.png" alt="">
+            <input type="text" placeholder="Search..">
+            <i class="fa-solid fa-magnifying-glass" style="color: grey" ></i>
+            <ul>
+                <li><a>Shop</a></li>
+                <li><a>Contact</a></li>
+                <li><a>About</a></li>
+                <li><a>Log In</a></li>
+                <li><a>Sign Up </a></li>
+            </ul>
+</header>
