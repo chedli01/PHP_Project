cf7ce97525f7e737b9e0db66fd777d0bd08bb305
Kousay Jebir <kousayjebir@gmail.com>
2024-03-30 16:07:09 +0100

    shopping cart

--- src/shopping-cart/add-to-cart.php
+++ src/shopping-cart/add-to-cart.php
@@ -2,6 +2,7 @@
     include_once "shopping-cart.php";
     session_start();
     function addToCart() {
+        if(isset($_POST['product-index'])){
         // LOOK FOR THE PRODUCT ADDED TO THE CART
         $productAdded = $_POST['product-index'];
         $products = $_SESSION['products']; 
@@ -10,8 +11,10 @@
         //add the item to the card with quantity 1
         $shoppingCard->addItem(array('product'=>$products[$productAdded],'quantity'=>1));
         $_SESSION['user_shopping_cart'] = serialize($shoppingCard);
-
+        }
     }
     addToCart();
+    header("Location: ../../public/product-list-page.php");
+    exit(); 
  ?>
 
