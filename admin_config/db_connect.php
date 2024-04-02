6ee22819afc3743d4c2e0a8e8565f7bd9cf3e25e
chedli01 <chedli.masmoudi01@gmail.com>
2024-04-02 02:47:31 +0200

    fixing_records

--- admin_config/db_connect.php
+++ admin_config/db_connect.php
@@ -3,8 +3,10 @@
 // Database connection parameters
 $servername = "localhost";
 $username = "root";
-$password = "123456"; // Assuming no password is set
-$database = "eCommercePHP"; // Change this to your database name
+// $password = "123456"; // Assuming no password is set
+$password="";
+// $database = "eCommercePHP"; // Change this to your database name
+$database="login_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
--- admin_config/records.php
+++ admin_config/records.php
@@ -19,52 +19,12 @@ if(isset($_SESSION["admin_id"])){
     <title>orders</title>
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th>orderId</th>
-                <th>customerId</th>
-                <th>orderDate</th>
-                <th>totalAmount</th>
-                <th>shippingAdress</th>
-                
-
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $sql="SELECT * FROM orders";
-            $result=$mysqli->query($sql);
-
-             while($row=$result->fetch_assoc()){
-                echo "<tr>
-                
-                <td>$row[orderId]</td>
-                <td>$row[customerId]</td>
-                <td>$row[orderDate]</td>
-                <td>$row[totalAmount]</td>
-                <td>$row[shippingAdress]</td>
-                <td><a href='/edit_orders.php?id=$row[orderId]'><button>Edit</button></a></td>
-                <td><a href='/delete_orders.php?id=$row[orderId]'><button>Delete</button></a></td>
-                <td><a href='/order_details.php?id=$row[orderId]'><button>View Deatils</button></a></td>
-
-            </tr>
-               
-
-
-
-
-
-                ";
-            }
-            ?>
-            
-            
-            
-            
-           
-        </tbody>
-    </table>
+<ul>
+                <li><a href="#" onclick="loadContent('pending.php')">pending</a></li>
+                <li><a href="#" onclick="loadContent('processing.php')">processing</a></li>
+                <li><a href="#" onclick="loadContent('shipped.php')">shipped</a></li>
+                <li><a href="#" onclick="loadContent('delivred.php')">delivred</a></li>
+            </ul>
     
 
     
