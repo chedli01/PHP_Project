83226e0b213815349c1f758c3e3d2f51655c0c31
YasmineZerai <zeraiyasmine7@gmail.com>
2024-04-10 22:40:25 +0100

    styled

--- public/shopping-cart-checkout.php
+++ public/shopping-cart-checkout.php
@@ -1,7 +1,7 @@
-<div class="shopping-cart">
+<div class="shopping-cart filters-sidebar" style="height:100vh; min-height:max-content; display:flex; justify-content:center">
             <!-- cart products section
              !-->
-        <div class="cart-items">
+        <div class="cart-items" style="gap:20px">
             <?php 
                 $productsInCart =$userShoppingCart->getProductsInCart();
                 foreach( $productsInCart as $product) {
@@ -10,9 +10,9 @@
              ?>
              <style>.cart-items img {width:50px;}</style>
         </div>
-        <div class="cart-summary">
+        <div class="cart-summary" >
             <!-- Summary of the shopping cart -->
-            <h2>Cart Summary</h2>
+            <h2 style="font-size:20px ;color:#DF9100">Cart Summary</h2>
             <p class="total-items summary">Total Items: <?php echo $userShoppingCart->getTotalItems(); ?></p>
             <p class="total-price summary">Total Price: <?php echo $userShoppingCart->getTotalToPay(); ?></p>
         </div>
