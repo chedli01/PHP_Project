2755d9b2b2ad2c8d28318d68df8f4d248084ca5b
YasmineZerai <zeraiyasmine7@gmail.com>
2024-04-10 22:38:54 +0100

    styled and fixed some bugs

--- public/checkout.php
+++ public/checkout.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../src/styles/global-styles.css">
+    <link rel="stylesheet" href="../src/styles/filter-options.css">
+    <link rel="stylesheet" href="../src/styles/product-card.css">
+    <link rel="stylesheet" href="../src/styles/search.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
-<body>
+<body class="product-list-body" style="height:100vh;  display:flex; flex-direction:column; justify-content:space-between">
+   
    <?php 
    session_start();
+   ;
    if(isset($_SESSION['user_id'])){
+   
    include "../src/shopping-cart/shopping-cart.php";
    $userShoppingCart = unserialize($_SESSION['user_shopping_cart']);
 
@@ -32,17 +40,28 @@
    } else {
        echo "User not found.";
    }
-   include "shopping-cart-checkout.php"; 
-   
-   //show the form
+    $PATH_TO_SHOP = "product-list-page.php";
+    $PATH_TO_LOGIN = "../Signup/login.php";
+    $PATH_TO_LOGOUT = "../Signup/logout.php";
+    $PATH_TO_SIGNUP = "../Signup/signup.php";
+    $PATH_TO_ABOUT = "../about.php";
+    $PATH_TO_CONTACT = "#";
+    include "..\header.php" ;
+    ?>
 
-   include "checkout-form.php";
+    <div class="checkout-container" style="display: flex; gap:50px">
+        <?php include "shopping-cart-checkout.php"; ?>
+        <?php include "checkout-form.php"; ?>
+    </div>
+
+<?php
 }
 else{
     echo "no user logged in please log in"; //error page no user is logged in 
 }
    ?>
 
-
+<?php include '..\footer.php' ?>
 </body>
+
 </html>
