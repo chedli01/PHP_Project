a1d796590a234df25aca6a80c4258879b82e37e7
YasmineZerai <zeraiyasmine7@gmail.com>
2024-04-10 23:55:15 +0100

    styled contact page

--- contact.php
+++ contact.php
@@ -4,28 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="style.css" rel="stylesheet"/>
 </head>
-<body>
-<form action="./src/send-email.php" method="post">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-
-    <label for="category">Category:</label>
+<body class="contact-body">
+    
+<form class="checkout-form" style="background-color:#b5aac4"action="./src/send-email.php" method="post">
+<h1>Contact Us</h1>
+   <div class="form-element"> <label class="label" for="name">Name:</label>
+    <input class="form-input" type="text" id="name" name="name" required>
+</div>
+   <div class="form-element"> <label class="label" for="email">Email:</label>
+    <input class="form-input" type="email" id="email" name="email" required>
+</div>
+   <div class="form-element"> <label class="label" for="category">Category:</label>
     <select id="category" name="category" required>
-        <option value="">Select a category</option>
+   <option value="">Select a category</option>
         <option value="Technical Issue">Technical Issue</option>
         <option value="Billing Inquiry">Billing Inquiry</option>
         <option value="General Feedback">General Feedback</option>
         <!-- Add more categories as needed -->
     </select>
+    </div>     
 
-    <label for="message">Message:</label>
+   <div class="form-element"> <label class="label" for="message">Message:</label>
     <textarea id="message" name="message" rows="4" required></textarea>
+    </div>
 
-    <button type="submit">Send Message</button>
+    <button class="signup-button" style="scale:95%" type="submit">Send Message</button>
 </form>
 
     
